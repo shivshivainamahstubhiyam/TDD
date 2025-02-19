d40bb494271f497a91558d01ca088f65d77d6433
shivshivainamahstubhiyam <parmarjatin5565@gmail.com>
2025-02-19 15:28:48 +0530

    Update src_string_calculator.py

--- src_string_calculator.py
+++ src_string_calculator.py
@@ -1,14 +1,22 @@
-def add(numbers):
-    if not numbers:
-        return 0
-    delimiter = ","
-    if numbers.startswith("//"):
-        parts = numbers.split("\n", 1)
-        delimiter = parts[0][2:]
-        numbers = parts[1]
-    numbers = numbers.replace("\n", delimiter)
-    nums = list(map(int, numbers.split(delimiter)))
-    negatives = [num for num in nums if num < 0]
-    if negatives:
-        raise ValueError(f"negative numbers not allowed: {','.join(map(str, negatives))}")
-    return sum(nums)
+import re
+
+class StringCalculator:
+    @staticmethod
+    def add(numbers: str) -> int:
+        if not numbers:
+            return 0
+        
+        delimiter = ','
+        if numbers.startswith("//"):
+            parts = numbers.split('\n', 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1]
+        
+        numbers = re.split(f'{delimiter}|\n', numbers)
+        numbers = [int(num) for num in numbers]
+
+        negatives = [num for num in numbers if num < 0]
+        if negatives:
+            raise ValueError(f"negative numbers not allowed: {', '.join(map(str, negatives))}")
+
+        return sum(numbers)
